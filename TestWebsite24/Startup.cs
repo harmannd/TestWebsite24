6cddee52001e90c3170416c8b4fa4dc7e49681ff
harmannd <derekhar2468@gmail.com>
2016-12-10 16:26:09 -0600

    Add project files.

---
+++ TestWebsite24/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestWebsite24.Startup))]
+namespace TestWebsite24
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
